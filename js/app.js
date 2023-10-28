433a194cf8c7100e647c43c06763b69482aaa55e
abrahan <abrahandev@gmail.com>
2023-10-28 11:26:04 +0200

    leyendo losm resultados con destructuring

--- js/app.js
+++ js/app.js
@@ -32,9 +32,27 @@ function iniciarApp() {
         const categoria = e.target.value
         const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`
         fetch(url)
-            .then(response => {
+            .then(response => response.json())
+            .then(resultado => mostrarRecetas(resultado.meals))
+    }
 
-            })
+    function mostrarRecetas(recetas = []) {
+        recetas.forEach(receta => {
+            const { idMeal, strMeal, strMealThumb } = receta
+
+            const recetaContenedor = document.createElement('DIV')
+            recetaContenedor.classList.add('col-md-4')
+
+            const recetaCard = document.createElement('DIV')
+            recetaCard.classList.add('card', 'mb-4')
+
+            const recetaImagen = document.createElement('IMG')
+            recetaImagen.classList.add('card-img-top')
+            recetaImagen.alt = `Imagen de la receta ${strMeal}`
+            recetaImagen.src = strMealThumb
+
+            console.log(recetaImagen)
+        })
     }
 }
 
